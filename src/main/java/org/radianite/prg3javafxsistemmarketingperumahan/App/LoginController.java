a23a89e70b83a4b187cea7ac60e67cc7f2079fc9
RaditSuryaWijaya <raditsurya022@gmail.com>
2024-06-13 14:51:12 +0700

    Comint

--- src/main/java/org/radianite/prg3javafxsistemmarketingperumahan/App/LoginController.java
+++ src/main/java/org/radianite/prg3javafxsistemmarketingperumahan/App/LoginController.java
@@ -2,6 +2,7 @@ package org.radianite.prg3javafxsistemmarketingperumahan.App;
 
 import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
+import javafx.animation.PauseTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
@@ -9,6 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -29,14 +31,16 @@ public class LoginController implements Initializable {
     private ImageView Image3;
     @FXML
     private ImageView Image4;
+    @FXML
+    private AnchorPane TextLogin;
+    @FXML
+    private Text imageLogin;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Set initial positions and opacities
         GroupWelcome.setTranslateX(-500);
         groupLoginFild.setTranslateY(-100);
-        labelLogin.setOpacity(0.0);
-        Image1.setOpacity(0.0);
         Image2.setOpacity(0.0);
         Image3.setOpacity(0.0);
         Image4.setOpacity(0.0);
@@ -72,16 +76,10 @@ public class LoginController implements Initializable {
 
         // Create FadeTransition for labelLogin
         FadeTransition fadeLabelLogin = new FadeTransition();
-        fadeLabelLogin.setNode(labelLogin);
+        fadeLabelLogin.setNode(TextLogin);
         fadeLabelLogin.setDuration(Duration.seconds(2));
         fadeLabelLogin.setFromValue(0.0);
-        fadeLabelLogin.setToValue(1.0
-        );
-        FadeTransition fadeOut1 = new FadeTransition();
-        fadeOut1.setNode(Image1);
-        fadeOut1.setDuration(Duration.seconds(1));
-        fadeOut1.setFromValue(1.0);
-        fadeOut1.setToValue(0.0);
+        fadeLabelLogin.setToValue(1.0);
 
         // Combine Translate and Fade transitions for GroupWelcome and groupLoginFild
         ParallelTransition parallelTransitionGroup = new ParallelTransition(
@@ -113,35 +111,48 @@ public class LoginController implements Initializable {
         fadeGambar4.setToValue(1.0);
 
         // Create FadeTransitions for fade-out
-
+        FadeTransition fadeOut1 = new FadeTransition();
+        fadeOut1.setNode(Image1);
+        fadeOut1.setDuration(Duration.seconds(3));
+        fadeOut1.setFromValue(1.0);
+        fadeOut1.setToValue(0.0);
 
         FadeTransition fadeOut2 = new FadeTransition();
         fadeOut2.setNode(Image2);
-        fadeOut2.setDuration(Duration.seconds(1));
+        fadeOut2.setDuration(Duration.seconds(5));
         fadeOut2.setFromValue(1.0);
         fadeOut2.setToValue(0.0);
 
         FadeTransition fadeOut3 = new FadeTransition();
         fadeOut3.setNode(Image3);
-        fadeOut3.setDuration(Duration.seconds(1));
+        fadeOut3.setDuration(Duration.seconds(5));
         fadeOut3.setFromValue(1.0);
         fadeOut3.setToValue(0.0);
 
         FadeTransition fadeOut4 = new FadeTransition();
         fadeOut4.setNode(Image4);
-        fadeOut4.setDuration(Duration.seconds(1));
+        fadeOut4.setDuration(Duration.seconds(5));
         fadeOut4.setFromValue(1.0);
         fadeOut4.setToValue(0.0);
 
         // Combine all image fade-in and fade-out transitions using SequentialTransition
         SequentialTransition sequentialTransition = new SequentialTransition(
-                fadeGambar2, fadeOut2, fadeGambar3, fadeOut3, fadeGambar4, fadeOut4
+                fadeGambar3, fadeGambar4, fadeOut4, fadeOut3
         );
         sequentialTransition.setCycleCount(SequentialTransition.INDEFINITE);
 
-        // Set event handler to start sequentialTransition after parallelTransitionGroup ends
-        parallelTransitionGroup.setOnFinished(event -> sequentialTransition.play());
+        // Create PauseTransition for delay
+        PauseTransition pause = new PauseTransition(Duration.seconds(2));
 
+        // Set event handler to start sequentialTransition and other events after pause
+        parallelTransitionGroup.setOnFinished(event -> pause.play());
+
+        pause.setOnFinished(event -> {
+            fadeGambar2.play();
+            sequentialTransition.play();
+            fadeOut1.play();
+            // Tambahkan event kedua di sini
+        });
 
         // Start transitions
         parallelTransitionGroup.play();
